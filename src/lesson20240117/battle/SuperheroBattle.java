f74ddaacac5143fe7f954d71caf1ef63bb31bb44
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-01-17 14:33:52 +0100

    Superheros

--- src/lesson20240117/battle/SuperheroBattle.java
+++ src/lesson20240117/battle/SuperheroBattle.java
@@ -2,6 +2,29 @@ package lesson20240117.battle;
 
 public class SuperheroBattle {
 
+    // Superhero battle
+    //
+    //Superhero class:
+    //- name String
+    //- health int
+    //- force int
+    //- Weapon class: - name String
+    //			    - force int
+    //- Protection class: - name String
+    //				    - level int
+    //
+    //методы:
+    //- static generateRandomSuperHero()
+    //- fight()  ----> health =  health - (force1 - force2) / level
+    //
+    //
+    //1. Создать 3 класса Superhero, Weapon, Protection
+    //2. Создать несколько персонажей SuperheroBattle main()
+    //3. Реализовать методы generateRandomSuperHero(), fight()
+    //fight(Superhero another)
+    //fight(Superhero one, Superhero second)
+    //
+    //4. Продемонстировать сражение между случайными персонажами
     public static void main(String[] args) {
         Weapon sword = new Weapon("Sword", 5);
         Protection helmet = new Protection("Helmet", 2);
@@ -13,6 +36,11 @@ public class SuperheroBattle {
 
         System.out.println(spiderman);
         System.out.println(batman);
+
+        Protection randomProtection = Protection.generateRandomProtection();
+        System.out.println(randomProtection);
+        System.out.println(Protection.generateRandomProtection());
+        System.out.println(Protection.generateRandomProtection());
     }
 
 
