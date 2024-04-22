e4099c9857664460dd375e8511537dbefa90b615
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-04-22 17:38:04 +0200

    synchronized

--- src/lesson20240422/CustomStack.java
+++ src/lesson20240422/CustomStack.java
@@ -30,23 +30,23 @@ public class CustomStack {
     }
 
     public static void main(String[] args) {
-//        CustomStack stack1 = new CustomStack();
-//        new Thread(() -> stack1.push("A")).start();
-//        new Thread(() -> stack1.push("B")).start();
-//
-//        CustomStack stack2 = new CustomStack();
-//        new Thread(() -> stack2.push("C")).start();
-//
-//
-//        while (true) {
-//            System.out.println("stack1 size: " + stack1.list.size());
-//            System.out.println("stack2 size: " + stack2.list.size());
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        CustomStack stack1 = new CustomStack();
+        new Thread(() -> stack1.push("A")).start();
+        new Thread(() -> stack1.push("B")).start();
+
+        CustomStack stack2 = new CustomStack();
+        new Thread(() -> stack2.push("C")).start();
+
+
+        for (int i = 0; i < 30; i++){
+            System.out.println("stack 1 size: " + stack1.list.size());
+            System.out.println("stack 2 size: " + stack2.list.size());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         ReentrantLock reentrantLock = new ReentrantLock();
         reentrantLock.lock();
